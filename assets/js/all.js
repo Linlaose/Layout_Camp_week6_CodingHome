4d35d493874b59b2cb2c51b8090ac3f9d206f209
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-08-18 01:28:37 +0800

    Update 2022-08-17T17:28:30.476Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,27 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+var swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 8,
+  spaceBetween: 16,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar'
+  },
+  mousewheel: {
+    invert: true
+  }
+});
 //# sourceMappingURL=all.js.map
