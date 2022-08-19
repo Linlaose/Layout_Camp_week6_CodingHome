f093809ee02af84104911930cd20cd556a3a9264
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-08-20 01:18:21 +0800

    完成：首頁完成最後輪播

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -8,6 +8,7 @@ const swiper = new Swiper('.swiper', {
   loop: true,
   slidesPerView: 8,
   spaceBetween: 16,
+  freeMode: true,
 
   // If we need pagination
   pagination: {
@@ -24,7 +25,43 @@ const swiper = new Swiper('.swiper', {
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+
+  // Mousewheel
   mousewheel: {
     invert: true,
+  },
+});
+
+const shareSwiper = new Swiper('.shareSwiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination-green',
+    type: 'bullets',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  // Autoplay
+  // autoplay: {
+  //   delay: 3000,
+  // },
+
+  // Fade
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
   },
 });
